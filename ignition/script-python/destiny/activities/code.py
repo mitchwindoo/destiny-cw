1fa5eca445ec9d58179eb0c08f49f8df254b7b8d
Mitch Landreth <mitchwindoo@gmail.com>
2022-03-15 22:27:42 +0000

    Fixed an issue where the activity instance was getting duplicated everytime new data was fetched.

--- ignition/script-python/destiny/activities/code.py
+++ ignition/script-python/destiny/activities/code.py
@@ -28,25 +28,31 @@ def ironBanner(membershipType,destinyMembershipId,characterId):
 			#print activities
 			for activity in activities:
 				# Check if the activity has already been recorded in the Database
-				queryParams = {'playerdestinyid':destinyMembershipId,'instanceid':activity['activityDetails']['instanceId']}
+				queryParams = {'playerdestinyid':str(destinyMembershipId),'instanceid':str(activity['activityDetails']['instanceId'])}
 				queryPath = 'destiny/activities/activityExists'
 				activityExists = system.db.runNamedQuery(queryPath, queryParams)
+				print "ironBanner: activityExists results for Activity ID " + str(activity['activityDetails']['instanceId']) + ": " + str(activityExists)
 				if activityExists == False:
-					#print "Activity does not exist in database"
+					print "ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " does not exist in database"
+					logger.info("ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " does not exist in database")
 					timestamp = datetime.strptime(activity['period'], '%Y-%m-%dT%H:%M:%SZ')
 					#print type(timestamp)
 					# Check if the activity in the returned data is in the current year, keeps the old data from re-populating
 					dataCurrent = system.date.isAfter(system.date.parse(timestamp), system.date.addMonths(system.date.now(), -3))
-					#print dataCurrent
 					if dataCurrent == True:
-						logger.info("ironBanner: Data is current")
+						print "ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " data is being added to the database"
+						logger.info("ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " data is being added to the database")
 						# Write Values to the Database
 						queryParams = {'playercharacterid':str(characterId),'playerdestinyid':str(destinyMembershipId),'instanceid':activity['activityDetails']['instanceId'],'modes':json.dumps(activity['activityDetails']['modes']),'mode':activity['activityDetails']['mode'],'isprivate':activity['activityDetails']['isPrivate'],'referenceid':activity['activityDetails']['referenceId'],'timestamp':timestamp,'activityduration':activity['values']['activityDurationSeconds']['basic']['displayValue'],'assists':activity['values']['assists']['basic']['value'],'avgscoreperkill':activity['values']['averageScorePerKill']['basic']['value'],'avgscoreperlife':activity['values']['averageScorePerLife']['basic']['value'],'completed':activity['values']['completed']['basic']['displayValue'],'completionreason':activity['values']['completionReason']['basic']['displayValue'],'deaths':activity['values']['deaths']['basic']['value'],'efficiency':activity['values']['efficiency']['basic']['value'],'fireteamid':activity['values']['fireteamId']['basic']['displayValue'],'kills':activity['values']['kills']['basic']['value'],'killsdeathsassists':activity['values']['killsDeathsAssists']['basic']['value'],'killsdeathsratio':activity['values']['killsDeathsRatio']['basic']['value'],'opponentsdefeated':activity['values']['opponentsDefeated']['basic']['value'],'score':activity['values']['score']['basic']['value'],'standing':activity['values']['standing']['basic']['displayValue'],'team':activity['values']['team']['basic']['value'],'teamscore':activity['values']['teamScore']['basic']['value'],'timeplayed':activity['values']['timePlayedSeconds']['basic']['displayValue']}
 						#print queryParams
 						queryPath = 'destiny/activities/addActivity'
 						system.db.runNamedQuery(queryPath, queryParams)
 					else:
-						logger.info("ironBanner: Skipping record due to age")
+						print "ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " being skipped due to age"
+						logger.info("ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " being skipped due to age")
+				else:
+					print "ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " Has already been logged to the database"
+					logger.info("ironBanner: Activity ID " + str(activity['activityDetails']['instanceId']) + " Has already been logged to the database")
 	except:
 		logger.error("API Call Failed for Member ID " + str(destinyMembershipId) + " Character " + str(characterId))
 		print "API Call Failed for Member ID " + str(destinyMembershipId) + " Character " + str(characterId)
